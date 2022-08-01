b0be87b154dbcbca591879b7c525f9d5f4cd9743
anumide <alabasamuelayomide3@gmail.com>
2022-08-01 05:11:24 +0100

    comsumption of API

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,7 @@
             <div class="flex my-6 overflow-hidden rounded-xl">
                 <input class="p-3 grow focus:outline-none text-sm" v-model="ip" type="text" placeholder="Search for any IP address or domain">
                 <div class="bg-black w-[12%] hover:bg-gray-800 focus:bg-slate-800 cursor-pointer transition-all duration-200 flex justify-center items-center"
-                @click="getIp">
+                @click="getIp" @keydown.enter="getIp">
                     <img src="../assets/images/icon-arrow.svg" alt="" class="w-1/5 h-1/4">
                 </div>
             </div>
@@ -24,15 +24,24 @@ import L from "leaflet";
  name: "Home",
  data(){
     return {
+        // https://api.db-ip.com/v2/free/self for getting ip address of client
         api_key: 'at_0UdHagaH9Uxou2gSySG8rdS3ijWVw',
         ip: '',
         addressObj: {},
+        data: [],
+        lat: null,
+        lng: null,
+        city: null,
+        country: null,
+        postalCode: null,
+        timezone: null,
+        ISP: null,
     }
  },
  methods: {
     
     initMap(){
-        var map = L.map('map').setView([51.505, -0.09], 13);
+        var map = L.map('map').setView([this.lat, this.lng], 17);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -40,24 +49,52 @@ import L from "leaflet";
 
         var myIcon = L.icon({
             iconUrl: '../src/assets/images/icon-location.svg',
-            iconSize: [25, 30],
+            iconSize: [30, 35],
             iconAnchor: [22, 64],
-            popupAnchor: [-10, -56],
+            popupAnchor: [-7, -56],
         });
-        L.marker([51.5, -0.09], {icon: myIcon}).addTo(map)
+        L.marker([this.lat, this.lng], {icon: myIcon}).addTo(map)
             .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
             .openPopup();
     },
     getIp(){
-        var url = 'https://geo.ipify.org/api/v2/country?apiKey=' + this.api_key + '&ipAddress=' + this.ip;
+        var url = 'https://geo.ipify.org/api/v2/country,city?apiKey=' + this.api_key + '&ipAddress=' + this.ip;
+            
         if(this.ip.length > 0){
-            fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                this.addressObj = data;
-                console.log(this.addressObj);
-            })
-            .catch(error => console.log(error));
+                
+            if(this.addressObj.length > 0){   // if addressObj is not empty, then check if the IP address has been fetched
+                 this.addressObj.forEach(e => {
+                    if(e.ip == this.ip){
+                        this.lat = e.location.lat;
+                        this.lng = e.location.lng;
+                        this.ISP = e.isp;
+                        this.timezone = 'UTC ' + e.location.timezone;
+                        this.city = e.location.city;
+                        this.country = e.location.country;
+                        this.postalCode = e.location.postalCode;
+                        console.log(this.lat, this.lng);
+                    }else{  // fetch the IP address if it hasn't been fetched yet
+                         fetch(url)
+                        .then(response => response.json())
+                        .then(data => {
+                            this.data.push(data)
+                            localStorage.data = JSON.stringify(this.data)
+                            this.addressObj = JSON.parse(localStorage.data)
+                        })
+                        .catch(error => console.log(error));
+                    }
+                 })
+            }else{
+                fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.data.push(data)
+                    localStorage.data = JSON.stringify(this.data)
+                    this.addressObj = JSON.parse(localStorage.data)
+                    console.log(this.addressObj);
+                })
+                .catch(error => console.log(error));
+            }
         }else{
             alert('please input IP address!')
         }
@@ -65,8 +102,23 @@ import L from "leaflet";
  },
  mounted(){
     this.initMap(); // initialize map on component mount
+     console.log(this.lat, this.lng, this.ISP, this.timezone, this.city, this.country, this.postalCode);
     
  },
+ created(){
+    this.addressObj = JSON.parse(localStorage.data)
+    // this.addressObj = data;
+    this.addressObj.forEach(e => {
+        this.lat = e.location.lat;
+        this.lng = e.location.lng;
+        this.ISP = e.isp;
+        this.timezone = 'UTC ' + e.location.timezone;
+        this.city = e.location.city;
+        this.country = e.location.country;
+        this.postalCode = e.location.postalCode;
+    })
+    console.log(this.addressObj);
+ }
 
 }
 </script>
